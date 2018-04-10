a7fd2eb5493917cf4be6f06d5abbabc801e5aad6
yedili <838702939@qq.com>
2018-04-10 20:00:40 +0800

    [section2-practiseA]:count same elements in the list

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,27 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PracticeA {
-    Map<String,Integer> countSameElements(List<String> collection1) {
+class PracticeA {
+    Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        HashMap<String, Integer> result = new HashMap<>();
+        collection1.forEach(str -> countElement(str, result));
 
-        return null;
+        return result;
+    }
+
+    private void countElement(String str, HashMap<String, Integer> result) {
+
+        if (result.containsKey(str)) {
+            int count = result.get(str);
+            count ++;
+            result.put(str, count);
+        }
+        else {
+            result.put(str, 1);
+        }
     }
 }
