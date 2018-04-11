3ed8ad4fd6a0c811059c2ca8467439c0d6d37744
yedili <838702939@qq.com>
2018-04-11 12:18:44 +0800

    [section3-practiseB]: return a new map with key and value

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        collectionA.forEach((key, count) -> subtractCount(key, count, object, collectionA));
+        return collectionA;
+    }
 
-        return null;
+    private void subtractCount(String key, Integer count, Map<String, List<String>> object, Map<String, Integer> collectionA) {
+
+        if (object.get("value").contains(key)) {
+            count -= count / 3;
+            collectionA.put(key, count);
+        }
     }
 }
